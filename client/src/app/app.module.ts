f6352d11d70e466a42cdceb3a9e7405f7b6e4e74
Daniel Epstein <epstein@ics.uci.edu>
2022-02-09 13:41:11 -0800

    Add FormsModule to the app's moduel

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { CarouselCardComponent } from './components/carousel-card/carousel-card.component';
@@ -31,6 +32,7 @@ import { TrackPageComponent } from './pages/track-page/track-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
